c386640baec5f434eb099777095d6c8c9dfe41ad
Saurabh <sbrahaspati21@gmail.com>
2022-01-30 22:30:09 +0530

    Initial commit

---
+++ Loginvalidation/Loginvalidation/src/P1/Employee.java
@@ -0,0 +1,44 @@
+package P1;
+
+public class Employee{  
+	private int userid;
+	private String username;  
+	private int userage;  
+	private String userdept;
+
+	public Employee(){}
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public int getUserage() {
+		return userage;
+	}
+
+	public void setUserage(int userage) {
+		this.userage = userage;
+	}
+
+	public String getUserdept() {
+		return userdept;
+	}
+
+	public void setUserdept(String userdept) {
+		this.userdept = userdept;
+	}
+
+	
+}
